24b4e0ff921307f8dee2a729fe1cfda3dcb58457
langsem <nikolai_langsem@live.com>
2019-04-02 19:15:11 +0200

    final push
    final push

---
+++ src/components/Titles.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Titles = () => (
+	<div>
+		<h1 className="title-container__title">Check forecast</h1>
+		<h3 className="title-container__subtitle">Check the weather</h3>
+	</div>
+);
+
+export default Titles;
